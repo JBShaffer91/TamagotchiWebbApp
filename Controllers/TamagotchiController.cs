ca402325685468d8e1818a988caee16e2f7cb17f
Justin Shaffer <justinbshaffer91@gmail.com>
2023-04-30 10:46:11 -0700

    Add views for Index and New actions

--- Controllers/TamagotchiController.cs
+++ Controllers/TamagotchiController.cs
@@ -4,22 +4,22 @@ using TamagotchiWebbApp.Models;
 
 public class TamagotchiController : Controller
 {
-  private static List<Tamagotchi> _tamagotchis = new List<Tamagotchi>();
-}
+    private static List<Tamagotchi> _tamagotchis = new List<Tamagotchi>();
 
-public IActionResult Index()
-{
-  return View(_tamagotchis);
-}
+    public IActionResult Index()
+    {
+        return View(_tamagotchis);
+    }
 
-public IActionResult New()
-{
-  return View();
-}
+    public IActionResult New()
+    {
+        return View();
+    }
 
-[HttpPost]
-public IActionResult Create(Tamagotchi tamagotchi)
-{
-  _tamagotchis.Add(tamagotchi);
-  return RedirectToAction(nameof(Index));
+    [HttpPost]
+    public IActionResult Create(Tamagotchi tamagotchi)
+    {
+        _tamagotchis.Add(tamagotchi);
+        return RedirectToAction(nameof(Index));
+    }
 }
--- Models/Tamagotchi.cs
+++ Models/Tamagotchi.cs
@@ -1,8 +1,14 @@
-public class Tamagotchi
+using System;
+
+namespace TamagotchiWebbApp.Models
 {
-  public int Id { get; set; }
-  public string Name { get; set; }
-  public int Food { get; set; }
-  public int Attention { get; set; }
-  public int Rest { get; set; }
+  public class Tamagotchi
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Food { get; set; }
+    public int Attention { get; set; }
+    public int Rest { get; set; }
+    public DateTime LastInteractedWith { get; set; }
+  }
 }
