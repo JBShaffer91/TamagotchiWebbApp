37e604113d77d3af691b5d3604963b2879d50140
Justin Shaffer <justinbshaffer91@gmail.com>
2023-04-30 11:13:25 -0700

    Add functionality to Feed, Play, and Sleep

--- Controllers/TamagotchiController.cs
+++ Controllers/TamagotchiController.cs
@@ -4,22 +4,43 @@ using TamagotchiWebbApp.Models;
 
 public class TamagotchiController : Controller
 {
-    private static List<Tamagotchi> _tamagotchis = new List<Tamagotchi>();
+  private static List<Tamagotchi> _tamagotchis = new List<Tamagotchi>();
 
-    public IActionResult Index()
-    {
-        return View(_tamagotchis);
-    }
+  public IActionResult Index()
+  {
+      return View(_tamagotchis);
+  }
 
-    public IActionResult New()
-    {
-        return View();
-    }
+  public IActionResult New()
+  {
+      return View();
+  }
 
-    [HttpPost]
-    public IActionResult Create(Tamagotchi tamagotchi)
-    {
-        _tamagotchis.Add(tamagotchi);
-        return RedirectToAction(nameof(Index));
-    }
+  [HttpPost]
+  public IActionResult Create(Tamagotchi tamagotchi)
+  {
+    _tamagotchis.Add(tamagotchi);
+    return RedirectToAction(nameof(Index));
+  }
+
+  [HttpPost]
+  public IActionResult Feed(int id)
+  {
+    _tamagotchis[id].Feed();
+    return RedirectToAction(nameof(Index));
+  }
+
+  [HttpPost]
+  public IActionResult Play(int id)
+  {
+    _tamagotchis[id].Play();
+    return RedirectToAction(nameof(Index));
+  }
+
+  [HttpPost]
+  public IActionResult Sleep(int id)
+  {
+    _tamagotchis[id].Sleep();
+    return RedirectToAction(nameof(Index));
+  }
 }
--- Models/Tamagotchi.cs
+++ Models/Tamagotchi.cs
@@ -5,10 +5,25 @@ namespace TamagotchiWebbApp.Models
   public class Tamagotchi
   {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int Food { get; set; }
     public int Attention { get; set; }
     public int Rest { get; set; }
     public DateTime LastInteractedWith { get; set; }
+
+    public void Feed()
+    {
+      Food = Math.Min(Food + 10, 100);
+    }
+
+    public void Play()
+      {
+      Attention = Math.Min(Attention + 10, 100);
+    }
+
+    public void Sleep()
+    {
+      Rest = Math.Min(Rest + 10, 100);
+    }
   }
 }
