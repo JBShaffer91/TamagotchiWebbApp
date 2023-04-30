e75189d9d9bb3962d2aab3e8b9d63e58f15f8a43
Justin Shaffer <justinbshaffer91@gmail.com>
2023-04-30 10:19:48 -0700

    Implement the Index, New, and Create actions

---
+++ Controllers/TamagotchiController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TamagotchiWebbApp.Models;
+
+public class TamagotchiController : Controller
+{
+  private static List<Tamagotchi> _tamagotchis = new List<Tamagotchi>();
+}
+
+public IActionResult Index()
+{
+  return View(_tamagotchis);
+}
+
+public IActionResult New()
+{
+  return View();
+}
+
+[HttpPost]
+public IActionResult Create(Tamagotchi tamagotchi)
+{
+  _tamagotchis.Add(tamagotchi);
+  return RedirectToAction(nameof(Index));
+}
---
+++ Models/Tamagotchi.cs
@@ -0,0 +1,8 @@
+public class Tamagotchi
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public int Food { get; set; }
+  public int Attention { get; set; }
+  public int Rest { get; set; }
+}
